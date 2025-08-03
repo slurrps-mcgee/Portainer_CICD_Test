87d148f091ebc564b814e3612089107dcbac0f24
slurrps-mcgee <slurrpsmcgee@gmail.com>
2025-08-03 14:52:06 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('<h1>Hello from GitHub CI/CD via Portainer 🚀</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
